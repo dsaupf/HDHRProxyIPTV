4ea55140f636107f74505caa115943a1d81f7679
dsaupf <dsaupf@users.noreply.github.com>
2019-04-09 12:02:29 +0200

    Add miliseconds to the TRACE Time

--- HDHRProxyIPTV/src/Trace.cpp
+++ HDHRProxyIPTV/src/Trace.cpp
@@ -81,7 +81,7 @@ void CTrace::ObtainCurrentDateTime(char* datetime, int size)
 	
 	GetLocalTime(&st);
 
-	_snprintf(datetime, size - 2, "%04d/%02d/%02d %02d:%02d:%02d", st.wYear, st.wMonth, st.wDay, st.wHour, st.wMinute, st.wSecond);
+	_snprintf(datetime, size - 2, "%04d/%02d/%02d %02d:%02d:%02d.%03d", st.wYear, st.wMonth, st.wDay, st.wHour, st.wMinute, st.wSecond, st.wMilliseconds);
 
 	//return dateTime;
 }
